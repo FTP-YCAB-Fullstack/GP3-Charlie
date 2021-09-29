dde021bf4354a7a5e25a20c7edfe17e8f82f841e
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2021-09-29 12:15:33 +0700

    user register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const {User} = require("../models");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const user = require("../models/user");
 
 let userController = {
     getAll : async (req,res) => {
@@ -17,7 +18,22 @@ let userController = {
 
     Register : async (req,res) => {
         try {
+            const {name,email,password,role} = req.body
             
+            const hashed = bcrypt.hashSync(password)
+
+            let data = {
+                name:name,
+                email:email,
+                password:hashed,
+                role:role
+            }
+            const newUser = await User.create(data)
+
+            res.status(201).json({
+                message : "user created",
+                teacher : newUser
+            })
         } catch (error) {
             console.log(error)
         }
--- routers/user.js
+++ routers/user.js
@@ -1,6 +1,8 @@
 const userController = require("../controllers/userController")
+const user = require("../models/user")
 const userRouter = require("express").Router()
 
 userRouter.get("/users",userController.getAll)
+userRouter.post("/users/register",userController.Register)
 
 module.exports = userRouter
