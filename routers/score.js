02922e987106cf0022843c53ec5cdd0a01e3d5b3
wirahadi007 <charlesotojr@gmail.com>
2021-10-02 15:37:49 +0800

    config role score

--- routers/score.js
+++ routers/score.js
@@ -1,12 +1,13 @@
 "use strict";
-
+const authentication = require("../middlewares/authentication")
+const authorization = require("../middlewares/authorization")
 const scoreController = require("../controllers/scoreController");
 const scoreRouter = require("express").Router();
 
-scoreRouter.get("/scores", scoreController.getScore);
-scoreRouter.get("/scores/:id", scoreController.getDetail)
-scoreRouter.post("/scores", scoreController.addScore);
-scoreRouter.patch("/scores/:id", scoreController.patchScore);
-scoreRouter.delete("/scores/:id", scoreController.deleteScore);
+scoreRouter.get("/scores",authentication,authorization(["admin","teacher"]), scoreController.getScore);
+scoreRouter.get("/scores/:id",authentication,authorization(["admin","teacher"]), scoreController.getDetail)
+scoreRouter.post("/scores",authentication,authorization(["admin"]), scoreController.addScore);
+scoreRouter.patch("/scores/:id",authentication,authorization(["admin"]), scoreController.patchScore);
+scoreRouter.delete("/scores/:id",authentication,authorization(["admin"]), scoreController.deleteScore);
 
 module.exports = scoreRouter;
